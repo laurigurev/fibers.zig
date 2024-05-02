55b9949f5c4de972736a261c91b0ec1ea32773e5
laurigurev <larigur@gmail.com>
2024-05-02 19:44:21 +0300

    zig bang

---
+++ src/fibers.zig
@@ -0,0 +1,9 @@
+// https://www.youtube.com/watch?v=HIVBhKj7gQU
+// https://swedishcoding.com/wp-content/uploads/2015/03/parallelizing_the_naughty_dog_engine_using_fibers.pdf
+// https://graphitemaster.github.io/fibers/
+
+// TODO:
+// - simple context switching
+// - coroutine api
+// - scheduling implementation
+// - multithreading (win32 threads and concurrency kit for primitives)
